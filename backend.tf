e8f7e403344c9fdcb6cc062dc9ebb0c9804c1deb
Dhuberto <dhuberto@gmail.com>
2026-08-06 15:53:32 -0300

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "danilo-terraform-backend-2026"
-#    key            = "terraform/atividade1/terraform.tfstate"
-#    region         = "us-east-1"
-#    use_lockfile   = true
-#  }
-#}
